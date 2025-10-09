9405428623367622cb66beeaaf00e6400da551a1
AdrianMPC <adrian.pinado@usil.pe>
2025-10-09 01:19:50 -0500

    functional name - icon

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:nubo_app/main.dart';
+import 'package:nubo/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
