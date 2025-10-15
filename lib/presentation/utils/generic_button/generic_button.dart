7cbedc710cbc5dce4d7bf2919182492c533161bf
AdrianMPC <adrian.pinado@usil.pe>
2025-10-14 22:46:35 -0500

    new file:   lib/presentation/utils/generic_button/generic_button.dart 	new file:   lib/presentation/utils/generic_button/generic_button_password.dart

---
+++ lib/presentation/utils/generic_button/generic_button.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+import 'package:nubo/config/config.dart';
+import 'package:flutter/services.dart';
+
+class UserField extends StatelessWidget {
+  const UserField({super.key,
+  this.labeltext,
+  this.hinttext,
+  this.icon,
+  this.text,
+  this.listinput,
+  this.validador});
+
+  final String? labeltext;
+  final String? hinttext;
+  final IconData? icon;
+  final TextInputType? text;
+  final List<TextInputFormatter>? listinput;
+  final String? Function(String?)? validador;
+
+  @override
+  Widget build(BuildContext context) {
+    final textFieldFocusNode = FocusNode();
+    final textTheme = Theme.of(context).textTheme;
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(
+          labeltext ?? '',
+          style: textTheme.displayMedium?.copyWith(color: gray400),     
+        ),
+        const SizedBox(height: 6),
+        TextFormField(
+          keyboardType: text,
+          inputFormatters: listinput,
+          focusNode: textFieldFocusNode,
+          style: textTheme.headlineMedium,
+          decoration:  InputDecoration(
+            floatingLabelBehavior: FloatingLabelBehavior.never, 
+            hintText: hinttext ,
+            hintStyle: textTheme.displayMedium?.copyWith(color: gray400), 
+            filled: true, // Needed for adding a fill color
+            fillColor: Colors.white, 
+            isDense: true,  // Reduces height a bit
+            prefixIcon: Icon(icon, size: 24, color: gray400),
+            suffixIcon: const Padding(
+              padding: EdgeInsets.fromLTRB(0, 0, 4, 0),
+            ),
+          ),
+            validator: validador,
+        ),
+      ],
+    );
+  }
+}
